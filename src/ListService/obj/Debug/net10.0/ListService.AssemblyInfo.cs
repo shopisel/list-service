fbb9c70e70e872233e7893178003f50f2bd47bbe
Pedro Gomes <pedromiguelgomes05@gmail.com>
2026-03-21 17:29:39 +0000

    changes ci-main.ymal

--- src/ListService/obj/Debug/net10.0/ListService.AssemblyInfo.cs
+++ src/ListService/obj/Debug/net10.0/ListService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("List service microservice")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b00c846ed00bb51950b204afbb636ced7b89ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b9f1e9df5c88070705866cabb5ac8498049b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
